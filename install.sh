3d4b5cf8d96138be42bed229447f36cbb09a5a29
ktgw0316 <ktgw0316@users.noreply.github.com>
2021-05-28 19:25:46 +0900

    Do not apt remove cmake (#963)

--- install.sh
+++ install.sh
@@ -37,9 +37,7 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install gcc-8 g++-8
 
 # Ubuntu 16.04 does not have newest CMake so need to build it manually
-if [[ `lsb_release -rs` != "18.04" ]]; then   
-  sudo apt-get --purge remove cmake-qt-gui -y
-  sudo apt-get --purge remove cmake -y
+if [[ `lsb_release -rs` < "18.04" ]]; then   
   mkdir -p cmake_tmp
   cd cmake_tmp
   wget https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
@@ -50,6 +48,7 @@ if [[ `lsb_release -rs` != "18.04" ]]; then
   sudo make install
   cd ../..
   sudo rm -r cmake_tmp
+  export PATH=/usr/local/bin:$PATH
 else
   sudo apt-get -y install cmake
 fi
